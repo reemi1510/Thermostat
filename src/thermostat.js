7bdf10a397e8532ae6b5c7dc9d0d78a1a41d1efb
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2019-03-07 16:49:20 +0000

    Added maximum temperatures

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,13 +4,18 @@ function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
   this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
 
   Thermostat.prototype.getCurrentTemperature = function () {
     return this.temperature;
   };
 
   Thermostat.prototype.up = function () {
-      this.temperature += 1;
+    if (this.isMaximumTemperature()) {
+      return
+    }
+    this.temperature += 1;
   };
 
   Thermostat.prototype.down = function () {
@@ -34,5 +39,12 @@ function Thermostat() {
 
   Thermostat.prototype.switchPowerSavingModeOn = function () {
     this.powerSavingMode = true;
+  };
+
+  Thermostat.prototype.isMaximumTemperature = function () {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
   };
 };
