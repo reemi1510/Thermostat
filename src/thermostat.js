479b3e248ead297c6199ae025eff8de2e40f4b6b
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2019-03-08 13:11:56 +0000

    Added energy usage output

--- src/thermostat.js
+++ src/thermostat.js
@@ -7,6 +7,7 @@ function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 
   Thermostat.prototype.getCurrentTemperature = function () {
     return this.temperature;
@@ -51,5 +52,15 @@ function Thermostat() {
 
   Thermostat.prototype.resetTemperature = function () {
     this.temperature = this.DEFAULT_TEMPERATURE;
+  };
+
+  Thermostat.prototype.energyUsage = function () {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   };
 };
