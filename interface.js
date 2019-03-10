6bd80e53e078c9cc589fd7636e01de015eb49c26
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2019-03-10 17:17:18 +0000

    Added JQuery

---
+++ interface.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  var thermostat = new Thermostat();
+  $('#temperature').text(thermostat.temperature);
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  })
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+})
